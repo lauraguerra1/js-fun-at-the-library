070318fb348c8340d3922788608dc39443699297
Laura Guerra <l.garciaguerra1@gmail.com>
2023-03-23 10:36:23 -0400

    Pass shelf tests

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,28 @@
-
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    shelf.unshift(book);
+  }
+}
+function unshelfBook(title, shelf) {
+  var matchingBook = shelf.find((book) => book.title === title);
+  var index = shelf.indexOf(matchingBook);
+  shelf.splice(index, 1);
+}
+function listTitles(shelf) {
+  var sentence = ``;
+  shelf.forEach((book, i) =>
+    i < shelf.length - 1
+      ? (sentence += `${book.title}, `)
+      : (sentence += book.title)
+  );
+  return sentence;
+}
+function searchShelf(shelf, title) {
+  return shelf.includes(shelf.find((book) => book.title === title));
+}
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf,
 };
