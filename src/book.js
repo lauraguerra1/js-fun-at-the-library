8508790513f9e090728b032b2e1bfb299689b20b
Laura Guerra <l.garciaguerra1@gmail.com>
2023-03-23 09:34:58 -0400

    Pass book tests

--- src/book.js
+++ src/book.js
@@ -1,12 +1,40 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`;
+}
+function buildMainCharacter(name, age, pronouns) {
+  var mainCharacter = {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  };
+  return mainCharacter;
+}
+function saveReview(review, reviewArr) {
+  if (!reviewArr.includes(review)) {
+    reviewArr.push(review);
+  }
+}
+function calculatePageCount(title) {
+  return title.length * 20;
+}
+function writeBook(title, mainCharacter, genre) {
+  var book = {
+    title: title,
+    mainCharacter: mainCharacter,
+    pageCount: calculatePageCount(title),
+    genre: genre,
+  };
+  return book;
+}
+function editBook(book) {
+  book.pageCount *= 0.75;
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
-}
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook,
+};
